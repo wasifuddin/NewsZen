668fe015007aee94780d40d73611e9b74cb697de
fateennr <nnaher123@gmail.com>
2024-10-12 17:47:40 +0600

    prothom alo

--- mzmin.py
+++ mzmin.py
@@ -3,55 +3,54 @@ from bs4 import BeautifulSoup
 import csv
 import os
 
-url = 'https://thefinancialexpress.com.bd/'
+url = 'https://mzamin.com/category.php?cat=4'
 
 def fetch_article_data(url):
     response = requests.get(url)
     
+
     if response.status_code != 200:
         print(f"Failed to retrieve data from {url}")
         return []
 
     soup = BeautifulSoup(response.content, 'html.parser')
     
-    articles = soup.find_all('article', class_='grid xl:grid-cols-2 gap-x-4 mb-4 last:mb-0')
+    articles = soup.find_all('div', class_='col-sm-8 col-7')
     article_data = []
 
     for article in articles:
-        h3_tag = article.find('h3')
-        
-        if h3_tag:
-            second_a_tag = h3_tag.find('a')
-            title_link = second_a_tag['href'] if second_a_tag else 'No link found'
-        else:
-            title_link = 'No link found'
+        title_tag = article.find('h4')
 
-        title_tag = article.find('p')
         if title_tag:
             title = title_tag.get_text(strip=True)
-        else:
-            title = 'No title found'
+            link = title_tag.find('a')['href']  # to fetch href attribute
 
-        article_data.append({
-            'title': title,
-            'link': title_link
-        })
+            # fetching the intro
+            intro_tag = article.find('p', class_='text-black-50')
+            intro = intro_tag.get_text(strip=True) if intro_tag else 'No introduction found'
+
+            article_data.append({
+                'title': title,
+                'link': link,
+                'date': intro
+            })
 
     return article_data
 
 def save_to_csv(articles):
     directory = 'websites'
-    filename = 'financialexpressbd.csv'
+    filename = 'mzamin.csv'
     
     os.makedirs(directory, exist_ok=True)
 
     file_path = os.path.join(directory, filename)
     
     with open(file_path, mode='w', newline='', encoding='utf-8') as file:
-        headers = ['title', 'link']
+        headers = ['title', 'link', 'date']
         writer = csv.DictWriter(file, fieldnames=headers)
         writer.writeheader()
         writer.writerows(articles)
+
 
 if __name__ == "__main__":
     articles = fetch_article_data(url)
@@ -60,8 +59,6 @@ if __name__ == "__main__":
 
     for article in articles:
         print(f"Title: {article['title']}")
-        print(f"Link: {url.rstrip('/') + article['link']}")  
+        print(f"Link: {url + article['link']}")  
+        print(f"Introduction: {article['date']}")
         print("-" * 40)
-
-
-
