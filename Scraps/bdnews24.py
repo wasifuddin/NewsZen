739af44677ccab931d8f1ce3f482469a27630f69
fateennr <nnaher123@gmail.com>
2024-11-10 20:29:20 +0600

    automated scrapper successful

--- Scraps/bdnews24.py
+++ Scraps/bdnews24.py
@@ -3,7 +3,9 @@ from bs4 import BeautifulSoup
 import csv
 import os
 
-url = 'https://bdnews24.com/archive'
+#title, imageurl, source, description, url, dateTime, topic
+
+url = 'https://bdnews24.com/'
 
 def fetch_article_data(url):
     response = requests.get(url)
@@ -15,56 +17,135 @@ def fetch_article_data(url):
 
     soup = BeautifulSoup(response.content, 'html.parser')
     
-    articles = soup.find_all('div', class_='SubCat-wrapper')
+    # articles = soup.find_all('div', class_='col-lg-3 col-md-6 order-lg-1 order-2')
     article_data = []
 
-    for article in articles:
+    news = soup.find_all('div', class_ = 'TopLeadList')
 
-        title_link = article.find('a')['href']
+    # for article in articles:
 
-        title_tag = article.find('h5')
-        publish_time = article.find('span', class_ = 'publish-time') # it is class_, not class
-        publish_time = publish_time.get_text(strip = True) if publish_time else 'No publish time'
+    for news_details in news:
 
-        catagory_arch = article.find('span', class_ = 'catagory-arch') # it is class_, not class
-        catagory_arch = catagory_arch.get_text(strip = True) if catagory_arch else 'No catagory'
+            title_link = news_details.find('a')['href']
 
-        if title_tag:
-            title = title_tag.get_text(strip=True)
-        
+            article_details_url = title_link if title_link.startswith('http') else 'No news link found'
 
-            article_data.append({
-                'title': title,
-                'link' : title_link,
-                'catagory' : catagory_arch,
-                'publish_time' : publish_time
-            })
+
+            # print("title link is ",title_link)
+
+            article_details = fetch_details(article_details_url)
+
+
+            if article_details :
+                article_data.append(article_details)
 
     return article_data
 
-def save_to_csv(articles):
-    directory = 'websites'
-    filename = 'bdnews24.csv'
+
+def fetch_details(article_url) :
+    response = requests.get(article_url)
+
+    news_details = []
+
+    if response.status_code != 200 :
+        print(f"failed to retrieve data from {article_url}")
+        return none
     
-    os.makedirs(directory, exist_ok=True)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+
+    # print("===="*10)
+
+    topic = soup.select_one('ul.ignore-print li:nth-child(2) a').text if soup.select_one('ul.ignore-print li:nth-child(2) a') else 'No topic found'
+    # print("the topic is ", topic)
+    
+
+    title = soup.find('div', class_ = 'd-flex live').find('h1').text.strip()
+    # print(title)
+
+    pic_tag = soup.find('picture')
+    imgsrc = ""
+
+    if pic_tag:
+        imgsrc = pic_tag.find('img', class_ = 'img-fluid')['src']
+        # print(imgsrc)
+    else :
+        print("No imgsrc")
+
+    # for description adding the text of all p tags
+
+    print_section = soup.find('div', class_='details-brief dNewsDesc print-section')
+
+    full_description = ""
+    if print_section :
+        description_section = print_section.find_all('p')
+
+        for descriptions in description_section:
+            # print(descriptions.get_text(strip = True))
+            full_description = full_description + "\n" + descriptions.get_text(strip = True)
+    else :
+        print("No description found")
+
+    # print(full_description)
+
+    pub_section = soup.find('div', class_ = 'pub-up print-section d-lg-flex')
+
+    if pub_section:
+        date_times = pub_section.find_all('span')
+        publish_time = date_times[1].get_text(strip = True)
+        # print("publish time is: ", publish_time)
+
+    # print("===="*10)
+
+    news_details = {
+        'title' : title,
+        'imageurl' : imgsrc,
+        'source' : 'bdnews24',
+        'description' : full_description,
+        'url' : article_url,
+        'dateTime' : publish_time,
+        'topic' : topic
+    }
+
+    return news_details
+
+
+def save_to_csv(articles):
+    print(articles)
+    directory = 'websites'
+    filename = 'scrapper_bdnews24.csv'
+    
+    os.makedirs(directory, exist_ok = True)
 
     file_path = os.path.join(directory, filename)
+
+    print(file_path)
     
     with open(file_path, mode='w', newline='', encoding='utf-8') as file:
-        headers = ['title', 'link', 'catagory','publish_time']
+        headers = ['title', 'imageurl', 'source' , 'description', 'url', 'dateTime','topic']
         writer = csv.DictWriter(file, fieldnames=headers)
         writer.writeheader()
         writer.writerows(articles)
 
 
 if __name__ == "__main__":
+
+    # fetch_article_data(url)
+
     articles = fetch_article_data(url)
 
     save_to_csv(articles)
 
-    for article in articles:
-        print(f"Title: {article['title']}")
-        print(f"Link: {url + article['link']}")  
-        print(f"Catagory: {url + article['catagory']}")  
-        print(f"Publication_time: {article['publish_time']}")
-        print("-" * 40)
+    # for article in articles:
+    #     print(f"title: {article['title']}")
+    #     print(f"imageurl: {article['imageurl']}")  
+    #     print(f"source: {article['source']}")  
+    #     print(f"description: {article['description']}")  
+    #     print(f"url: {article['url']}")  
+    #     print(f"dateTime: {article['dateTime']}")  
+    #     print(f"topic: {article['topic']}")
+    #     print("-" * 40)
+
+
+
+    
