8d92b828ede4f10e2bb126bb45e9a21c0a491d00
Rafiul Arefeen Islam <rafiularefeen@iut-dhaka.edu>
2025-03-13 23:04:32 +0600

    Made the search results screen better, reverted home screen to previous commit

--- lib/features/main/presentation/search_screen/search_screen.dart
+++ lib/features/main/presentation/search_screen/search_screen.dart
@@ -11,6 +11,7 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: main_background_colour,
         title: const Text(
